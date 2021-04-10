c2fd9276dcad21e8956f1552cbda89ff0c39ed5b
SookyeongLee <74999421+SookyeongLee@users.noreply.github.com>
2021-04-10 22:08:45 +0900

    HTML + CSS +JavaScript
    JavaScript 기능
    1. input에 작성한 text를 +버튼과 엔터를 통해 추가
    2. 클릭시 삭제 가능

--- DOM/Shopping List/main.js
+++ DOM/Shopping List/main.js
@@ -1,2 +1,38 @@
-//쇼핑 항목 삭제
+const addBtn = document.querySelector('.shopping__btn');
+const input = document.querySelector('.shopping__input');
+const items = document.querySelector('.items');
+const trashBox = document.querySelector('.trashBox');
 const trash = document.querySelector('.trash');
+
+addBtn.addEventListener('click', updateItem());
+// input.addEventListener('keydown', () => {
+//     if (keyCode === 13){
+//         updateItem();
+//     }       
+// });
+
+function updateItem() {
+    
+    input.addEventListener('change', (e) => {
+        //Add item               
+        const item = document.createElement('li');
+        item.setAttribute('class', 'item');
+        item.textContent = e.target.value;
+        items.appendChild(item);
+
+        //Add trash icon        
+        const trash = document.createElement('li');
+        trash.setAttribute('class', 'trash');
+        const icon = document.createElement('i');
+        icon.setAttribute('class', 'fas fa-trash-alt');
+        trash.appendChild(icon);
+        trashBox.appendChild(trash);
+    });
+}
+
+trashBox.addEventListener('click', (e) => {
+    deleteTrash = e.target.parentNode;
+    console.log(deleteTrash); //li
+    console.log(e.target); //i
+    trashBox.removeChild(deleteTrash);   
+});
